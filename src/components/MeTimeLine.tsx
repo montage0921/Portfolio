bbf78320880fe59c71f6611797f8ecc50fb6d272
unknown <garyshi0921@gmail.com>
2025-10-06 23:27:32 -0400

    finish timeline

--- src/components/MeTimeLine.tsx
+++ src/components/MeTimeLine.tsx
@@ -5,6 +5,7 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import TimelineTag from "../utility/TimelineTag";
 import TimelineIcon from "../utility/TimelineIcon";
+import timeline from "../data/timeline.json";
 
 export default function MeTimeLine() {
   return (
@@ -18,49 +19,50 @@ export default function MeTimeLine() {
         }}
       >
         <VerticalTimeline lineColor="hsl(250 47% 60%)" layout="1-column-left">
-          <VerticalTimelineElement
-            contentStyle={{
-              background: "hsl(var(--card-bg))",
-              color: "hsl(var(--card-text))",
-              boxShadow: "none",
-              borderRadius: "12px",
-              width: "440px",
-              paddingBottom: "0px",
-              paddingRight: "25px",
-            }}
-            contentArrowStyle={{
-              borderRight: "7px solid  hsl(var(--card-bg))",
-            }}
-            iconStyle={{
-              boxShadow: "none",
-            }}
-            icon={<TimelineIcon imgName="blinkle.jpg" />}
-          >
-            <div className="flex flex-col gap-1">
-              <h3 className="text-xl font-semibold">
-                Software Engineer Intern
-              </h3>
-              <div className="flex gap-3 text-[hsl(var(--date-color))] font-semibold">
-                <h4>NY, USA</h4>
-                <h4 className="">2025.03-2025.10</h4>
-                <h4>Blinkle AI</h4>
-              </div>
+          {timeline.map((t) => (
+            <VerticalTimelineElement
+              contentStyle={{
+                background: "hsl(var(--card-bg))",
+                color: "hsl(var(--card-text))",
+                boxShadow: "none",
+                borderRadius: "12px",
+                width: "460px",
+                paddingBottom: "0px",
+                paddingRight: "25px",
+              }}
+              contentArrowStyle={{
+                borderRight: "7px solid  hsl(var(--card-bg))",
+              }}
+              iconStyle={{
+                boxShadow: "none",
+              }}
+              icon={<TimelineIcon imgName={t.img} />}
+            >
+              <div className="flex flex-col gap-1">
+                <h3 className="text-xl font-semibold">{t.title}</h3>
 
-              <div className="flex flex-wrap gap-2 mt-1">
-                <TimelineTag text={"Full-Stack Developemnt"} />
-                <TimelineTag text={"TypeScript"} />
-                <TimelineTag text={"React"} />
-                <TimelineTag text={"Node JS"} />
+                <div className="flex gap-3 text-[hsl(var(--date-color))] font-semibold">
+                  {t.organization && <h4>{t.organization}</h4>}
+                  {t.location && <h4>{t.location}</h4>}
+                  <h4 className="">{t.date}</h4>
+                </div>
+
+                {t.skills && (
+                  <div className="flex flex-wrap gap-2 mt-1">
+                    {t.skills.map((s) => (
+                      <TimelineTag text={s} />
+                    ))}
+                  </div>
+                )}
+
+                <div className="">
+                  <p className="text-base !font-normal text-justify">
+                    {t.description}
+                  </p>
+                </div>
               </div>
-              <div className="">
-                <p className="text-base !font-normal text-justify">
-                  Worked with cross-functional teams to design, implement, and
-                  optimize full-stack features for an innovative
-                  resume-tailoring and job-board platform.
-                </p>
-              </div>
-            </div>
-          </VerticalTimelineElement>
+            </VerticalTimelineElement>
+          ))}
         </VerticalTimeline>
       </div>
     </div>

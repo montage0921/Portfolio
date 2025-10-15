afcbe8174e0a6f3ddfa299f4c788a4eed0e88364
unknown <garyshi0921@gmail.com>
2025-10-14 21:26:29 -0400

    hide/show navbar based on scrolling and mouse event

--- src/sections/NavBar.tsx
+++ src/sections/NavBar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Logo from "../components/Logo";
 import Nav from "../components/Nav";
 import DarkMode from "../components/DarkMode";
@@ -6,7 +7,41 @@ import { Menu, X } from "lucide-react";
 import { sections } from "../data/sections";
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); //for mobile version burger menu
+  const [isVisible, setIsVisible] = useState(true); //hide when not scroll
+
+  // navbar visible when scrolling, and hidden when not
+  useEffect(function () {
+    let scrollTimer: number;
+    function handleScroll() {
+      setIsVisible(true);
+      clearTimeout(scrollTimer);
+      // disappear after 1 second so user has time to click on it
+      // stay if scroll bar is at top
+      scrollTimer = window.setTimeout(() => {
+        if (window.scrollY > 100) setIsVisible(false);
+        else setIsVisible(true);
+      }, 800);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(function () {
+    function handleMouseEnterOrLeaveTop(e: MouseEvent) {
+      if (e.clientY < 50) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    }
+
+    window.addEventListener("mousemove", handleMouseEnterOrLeaveTop);
+    return () =>
+      window.removeEventListener("mousemove", handleMouseEnterOrLeaveTop);
+  }, []);
+
   function handleScollIntoSection(id: string) {
     const section = document.getElementById(id);
     if (id === "home") {
@@ -19,7 +54,13 @@ export default function NavBar() {
   }
 
   return (
-    <div className="px-2 flex items-center justify-between md:px-8 py-4">
+    <div
+      className={`px-2 flex items-center justify-between md:px-8 py-4 transition-opacity duration-500 ${
+        isVisible
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
+      }`}
+    >
       <Logo />
       <Nav />
       <div className="flex gap-2">
